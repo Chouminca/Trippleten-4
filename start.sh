e5fb8af0904fbd04b1f1f4d4570a83e66120a1e4
Chouminca Otero <chouminkafertile@Chouminkas-MacBook-Air.local>
2024-09-12 00:16:23 -0400

    update spelling

--- start.sh
+++ start.sh
@@ -4,18 +4,18 @@ echo "Current directory: $(pwd)"
 
 ls -la
 
-echo "Navigating to project directory: /Users/chouminkafertile/trippleten-4/"
-cd /Users/chouminkafertile/trippleten-4/
+echo "Navigating to project directory: /Users/chouminkafertile/trippleten-4"
+cd /Users/chouminkafertile/trippleten-4
 
 echo "After cd, Current directory: $(pwd)"
 
 ls -la
 
-echo "Listing directories in /Users/chouminkafertile/trippleten-4/"
-ls -la /Users/chouminkafertile/trippleten-4/
+echo "Listing directories in /Users/chouminkafertile/trippleten-4"
+ls -la /Users/chouminkafertile/trippleten-4
 
-echo "Listing directories in /Users/chouminkafetrile/tripletten-4/projectenv/bin/"
-ls -la /Users/chouminkafertile/trippleten-4/projectenv/bin/
+echo "Listing directories in /Users/chouminkafetrile/tripletten-4/projectenv/bin"
+ls -la /Users/chouminkafertile/trippleten-4/projectenv/bin
 
 echo "Activating virtual environment"
 /bin/bash -c "source /Users/chouminkafertile/trippleten-4/projectenv/bin/activate"
