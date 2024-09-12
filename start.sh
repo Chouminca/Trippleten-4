a0fe903f5c37592aabb293b930818cea0536dec7
Chouminca Otero <chouminkafertile@Chouminkas-MacBook-Air.local>
2024-09-12 10:55:04 -0400

    fixed issues for render

--- start.sh
+++ start.sh
@@ -4,21 +4,21 @@ echo "Current directory: $(pwd)"
 
 ls -la
 
-echo "Navigating to project directory: /Users/chouminkafertile/Trippleten-4"
-cd /Users/chouminkafertile/Trippleten-4
+echo "Navigating to project directory: /Users/chouminkafertile/Trippleten-4/"
+cd /Users/chouminkafertile/Trippleten-4/
 
 echo "After cd, Current directory: $(pwd)"
 
 ls -la
 
-echo "Listing directories in /Users/chouminkafertile/Trippleten-4"
-ls -la /Users/chouminkafertile/Trippleten-4
+echo "Listing directories in /Users/chouminkafertile/Trippleten-4/"
+ls -la /Users/chouminkafertile/Trippleten-4/
 
-echo "Listing directories in /Users/chouminkafetrile/Tripletten-4/projectenv/bin"
-ls -la /Users/chouminkafertile/Trippleten-4/projectenv/bin
+echo "Listing directories in /Users/chouminkafetrile/Tripletten-4/projectenv/bin/"
+ls -la /Users/chouminkafertile/Trippleten-4/projectenv/bin/
 
 echo "Activating virtual environment"
-/bin/bash -c "source /Users/chouminkafertile/Trippleten-4/projectenv/bin/activate"
+/bin/bash -c "source /Users/chouminkafertile/Trippleten-4/projectenv/bin/activate/"
 
 echo "Running Streamlit app"
 streamlit run app.py
