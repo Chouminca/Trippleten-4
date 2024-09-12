7d8992d87cba9926b82d47696147a15a6bf96794
Chouminca Otero <chouminkafertile@Chouminkas-MacBook-Air.local>
2024-09-11 23:38:50 -0400

    add start for render

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -e
 
+cd /Users/chouminkafertile/trippleten-4/
+
+ls -la /Users/chouminkafertile/trippleten-4/
+ls -la /Users/chouminkafertile/trippleten-4/projectenv/bin/
+
+source /Users/chouminkafertile/trippleten-4/projectenv/bin/activate
+
+streamlit run app.py
