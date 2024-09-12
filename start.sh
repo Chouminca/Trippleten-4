e356e4a42b7743a98035ae2a5be719ace7d2b76e
Chouminca Otero <chouminkafertile@Chouminkas-MacBook-Air.local>
2024-09-12 00:03:10 -0400

    Updated start.sh for better debugging

--- start.sh
+++ start.sh
@@ -1,10 +1,24 @@
 set -e
 
+echo "Current directory: $(pwd)"
+
+ls -la
+
+echo "Navigating to project directory: /Users/chouminkafertile/trippleten-4/"
 cd /Users/chouminkafertile/trippleten-4/
 
+echo "After cd, Current directory: $(pwd)"
+
+ls -la
+
+echo "Listing directories in /Users/chouminkafertile/trippleten-4/"
 ls -la /Users/chouminkafertile/trippleten-4/
+
+echo "Listing directories in /Users/chouminkafetrile/tripletten-4/projectenv/bin/"
 ls -la /Users/chouminkafertile/trippleten-4/projectenv/bin/
 
-source /Users/chouminkafertile/trippleten-4/projectenv/bin/activate
+echo "Activating virtual environment"
+/bin/bash -c "source /Users/chouminkafertile/trippleten-4/projectenv/bin/activate"
 
+echo "Running Streamlit app"
 streamlit run app.py
